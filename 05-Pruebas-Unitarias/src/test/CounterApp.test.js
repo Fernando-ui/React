d2c0e637679b2fe40552ab4403c535f0eae5ec22
fersan <fst-96@hotmail.com>
2021-08-28 17:15:41 -0500

    Anadiendo fin de seccion pruebas

--- 05-Pruebas-Unitarias/src/test/CounterApp.test.js
+++ 05-Pruebas-Unitarias/src/test/CounterApp.test.js
@@ -1,6 +1,71 @@
-describe('', () => {
-    test('should ', () => {
+import {shallow} from 'enzyme';
+import CounterApp from '../CounterApp';
+
+
+describe('Pruebas con el conunter App', () => {
+
+    let wrapper = shallow(<CounterApp value={0}/>);
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp value={0}/>);
+    })
+
+
+    test('Debera mostrar <CounterApp /> correctamente  ', () => {
+        
+        const valor = 10;
+
+        expect(wrapper).toMatchSnapshot();
+
+    });
+    test('Debera mostrar <CounterApp /> correctamente con el valor de 100  ', () => {
+        
+        const valor = 100;
+        const wrapper = shallow(<CounterApp value={100} />);
+        const valorDeCounter = wrapper.find('h2').text();
+
+        expect(valorDeCounter).toBe('100');
+
+    });
+
+    test('Debe de incrementar con el boton de +1 ', () => {
+
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        console.log(counterText);
+
+        expect(counterText).toBe('1');
+
+    })
+    test('Debe de incrementar con el boton de -1 ', () => {
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        console.log(counterText);
+
+        expect(counterText).toBe('-1');
+
+    })
+    test('Debe de incrementar con el boton de +1 ', () => {
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        console.log(counterText);
+        
+        expect(counterText).toBe('-1');
         
     })
     
-})
+    
+    test('Debe de colocar el valro por defecto con el boton reset   ', () => {
+        
+        const wrapper = shallow(<CounterApp value={105} />);
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('105');
+        console.log(counterText);
+
+    })
+    
+});
--- 05-Pruebas-Unitarias/src/test/PrimerComponente.test.js
+++ 05-Pruebas-Unitarias/src/test/PrimerComponente.test.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import {shallow, configure} from 'enzyme';
+import {shallow} from 'enzyme';
 import PrimerComponente from "../PrimerComponente";
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import '@testing-library/jest-dom';
-import {createSerializer} from 'enzyme-to-json';
 
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
-
-configure({adapter: new Adapter()})
 describe('Pruebas en <PrimerComponente />', () => {
 
     
     test('Debe de mostrar  <PrimerComponente /> correctamente', () => {
-        const saludo = 'Hola, Soy Fernankkdo';
+        const saludo = 'Hola, Soy Fernando';
         const wrapper = shallow(<PrimerComponente saludo={saludo}/>);
         
         expect(wrapper).toMatchSnapshot();
