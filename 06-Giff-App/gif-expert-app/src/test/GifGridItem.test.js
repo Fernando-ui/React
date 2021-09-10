8c9c9f9a3da639ca6afe039e2a702f8e6765d585
fersan <fst-96@hotmail.com>
2021-09-09 21:29:42 -0500

    Anadiendo test parte 1

--- 06-Giff-App/gif-expert-app/src/test/GifGridItem.test.js
+++ 06-Giff-App/gif-expert-app/src/test/GifGridItem.test.js
@@ -6,13 +6,31 @@ import { GifGridItem } from '../components/GifGridItem';
 
 describe('Pruebas en el componente <GifGridItem />', () => {
 
+    // * Estos son los argumentos que le enviaremos a nuestro componente
+    const title = 'Un titulo';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={ title } url={ url} />);
+
     test('Debe de mostrar el componente correctamente', () => {
         
-
-        const wrapper = shallow(<GifGridItem />);
-
         expect(wrapper).toMatchSnapshot();
         
     });
+
+    test('Debe de tener un parrafo con el titulo', () => {
+        
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+
+
+    });
+    
+    test('Debe de tener una imagen igual al url y alr de los props', () => {
+        
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+    
     
 })
