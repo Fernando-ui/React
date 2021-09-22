ce4195cd6ecd149de48296a7cc01b0f90dca1b23
Fernando Sosa <fernando.sosa@grupobituaj.com>
2021-09-22 16:05:11 -0500

    A-useMemo

---
+++ 07-hookGenerales/app-hook-general/src/helpers/ProcesoPesado.js
@@ -0,0 +1,11 @@
+
+export const procesoPesado = ( iteraciones ) => {
+
+    for(let i = 0; i < iteraciones; i++){
+
+        console.log('Vamos bien');
+        
+    }
+
+    return `${iteraciones} iteraciones realizadas.`
+}
