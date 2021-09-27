cc263ae3d8b387e0b58c3ca4402633f857329039
Fernando Sosa <fernando.sosa@grupobituaj.com>
2021-09-27 11:56:34 -0500

    inicio UseContext

---
+++ 07-hookGenerales/app-hook-general/src/components/09-useContext/MainApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AppRouter } from './AppRouter';
+
+
+export const MainApp = () => {
+    return (
+    <div>
+        <h1>MainApp Screen</h1>
+        <hr/>
+        <AppRouter/>
+    </div>
+    )
+}
