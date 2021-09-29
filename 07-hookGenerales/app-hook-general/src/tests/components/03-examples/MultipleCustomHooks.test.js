a338b4afdb491daafcacd922d9af9c6fa9b9cae2
Fernando <ferstxd@gmail.com>
2021-09-29 15:39:20 -0500

    Pruebas con multiples hooks simultaneos

--- 07-hookGenerales/app-hook-general/src/tests/components/03-examples/MultipleCustomHooks.test.js
+++ 07-hookGenerales/app-hook-general/src/tests/components/03-examples/MultipleCustomHooks.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { MultipleCustomHooks } from '../../../components/03-examples/MultipleCustomHooks';
 import { useFetch } from '../../../hooks/useFetch';
+import { useCounter } from '../../../hooks/useCounter';
+
 jest.mock('../../../hooks/useFetch');
+jest.mock('../../../hooks/useCounter');
+
 
 describe('Pruebas en <MultipleCustomHooks/>', () => {
+
+    useCounter.mockReturnValue({
+
+        counter:10,
+        increment:()=>{}
+    });
     
     test('Debe de mostrarse correctamente', () => {
         
@@ -33,7 +43,11 @@ describe('Pruebas en <MultipleCustomHooks/>', () => {
             error:null
         });
 
+        expect(wrapper.find('.alert').exists()).toBe(false);
+        expect(wrapper.find('.mb-0').text().trim()).toBe('Hola mundo');
+        expect(wrapper.find('footer').text().trim()).toBe('Fernando');
         const wrapper = shallow( <MultipleCustomHooks />);
+
         console.log(wrapper.html());
         
 
