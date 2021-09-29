3b855b5d8d1c52462b794bc13f094fdb26e455f7
Fernando <ferstxd@gmail.com>
2021-09-29 15:16:22 -0500

    C-monimos

--- 07-hookGenerales/app-hook-general/src/tests/components/03-examples/MultipleCustomHooks.test.js
+++ 07-hookGenerales/app-hook-general/src/tests/components/03-examples/MultipleCustomHooks.test.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { MultipleCustomHooks } from '../../../components/03-examples/MultipleCustomHooks';
+import { useFetch } from '../../../hooks/useFetch';
+jest.mock('../../../hooks/useFetch');
 
 describe('Pruebas en <MultipleCustomHooks/>', () => {
     
     test('Debe de mostrarse correctamente', () => {
-   
+        
+        useFetch.mockReturnValue({
+
+            data:null,
+            loading:true, 
+            error:null
+        });
         
         const wrapper = shallow(<MultipleCustomHooks />);
         expect(wrapper ).toMatchSnapshot();
 
+
+    });
+    
+    test('Debera debe de mostar la informacion', () => {
+        
+        useFetch.mockReturnValue({
+
+            data:[{
+                author:'Fernando', 
+                quote:'Hola mundo'
+            }],
+            loading:true, 
+            error:null
+        });
+
+        const wrapper = shallow( <MultipleCustomHooks />);
+        console.log(wrapper.html());
+        
 
     });
     
