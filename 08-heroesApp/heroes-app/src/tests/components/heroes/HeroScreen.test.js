dc4654811138ae8d009601babd18f6e240d1d79f
Fernando <ferstxd@gmail.com>
2021-10-13 16:29:25 -0500

    Anadiendo prueba heroScreen

--- 08-heroesApp/heroes-app/src/tests/components/heroes/HeroScreen.test.js
+++ 08-heroesApp/heroes-app/src/tests/components/heroes/HeroScreen.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { HeroScreen } from '../../../components/heroes/HeroScreen';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Route } from 'react-router-dom';
 
 
 describe('Pruebas en <HeroScreen/>', () => {
@@ -13,17 +13,81 @@ describe('Pruebas en <HeroScreen/>', () => {
         goBack:jest.fn(),
     }
 
-    const wrapper = mount(
-        <MemoryRouter initialEntries={['/hero']}>
-            <HeroScreen history={ history }/>
-
-        </MemoryRouter>
-    );
-
+    
     test('Debe de moastar el componente redirect si no hay argumentos en el URL', () => {
-       
-        expect(wrapper.find('Redirect').exists()).toBe(true);
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                <HeroScreen history={ history }/>
+                <Route path="/hero/:heroeId" component={HeroScreen}/>
+            </MemoryRouter>
+        );
+        expect(wrapper.find('.row').exists()).toBe(true);
     });
     
+    test('debe de regresar a la  pantalla anterior con push', () => {
+        
+        const history = {
 
+            length:1,
+            push:jest.fn(),
+            goBack:jest.fn(),
+        }
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                <HeroScreen history={ history }/>
+                <Route 
+                path="/hero/:heroeId" 
+                component={ (props) => <HeroScreen history={history}/>}
+                
+                />
+            </MemoryRouter>
+        );
+
+        wrapper.find('button').prop('onClick')();
+        expect(history.push).toHaveBeenCalledWith('/');
+        expect(history.goBack).not.toHaveBeenCalled();
+
+    });
+
+    test('debe de regresar a la pantalla anterior GOBACK', () => {
+        
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                <HeroScreen history={ history }/>
+                <Route 
+                path="/hero/:heroeId" 
+                component={ (props) => <HeroScreen history={history}/>}
+                
+                />
+            </MemoryRouter>
+        );
+
+        wrapper.find('button').prop('onClick')();
+        expect(history.push).toHaveBeenCalledTimes(0);
+        expect(history.goBack).toHaveBeenCalled();
+
+    });
+
+    test('debe de lamar el redirect si el heroe no existe', () => {
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                <HeroScreen history={ history }/>
+                <Route 
+                path="/hero/:heroeId" 
+                component={ (props) => <HeroScreen history={history}/>}
+                
+                />
+            </MemoryRouter>
+        );
+        
+        console.log(wrapper.text('Redirect').toBe(''));
+        
+    })
+    
+    
+    
 });
