5109b373ffa8c4697dc390ae8a36e9e99ada6e42
Fernando <ferstxd@gmail.com>
2021-10-18 12:02:39 -0500

    Anadiendo mensajes de error

--- 09-journal-app/src/components/auth/RegistrerScreen.js
+++ 09-journal-app/src/components/auth/RegistrerScreen.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useForm } from '../../hooks/useForm'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import validator from 'validator'
+
+
 
 export const RegistrerScreen = () => {
 
@@ -18,7 +21,10 @@ export const RegistrerScreen = () => {
     const handleRegister = (e) => {
         e.preventDefault();
         
-        console.log(name,email,password,password2);
+        if( isFormValid()){
+            console.log('Formulario correcto');
+            
+        }
         
     
     }
@@ -26,18 +32,37 @@ export const RegistrerScreen = () => {
     
 const isFormValid = () => {
 
+    if( name.trim().length === 0){
+
+        console.log('Name is required');
+        return false;
+
+    }else if( !validator.isEmail(email)){
+
+        console.log('El email no es valido');
+        return false;
+
+    }else if( password !== password2 || password.length < 5){
+        console.log('Password should be at least 6 chatacteres and match each other');
+        
+        return false;
+    }
+
+   
     
+    return true;
 
 }
-
-
-
-
 
     return (
         <>
         <h3 className="auth__title">Register</h3>
         <form onSubmit={handleRegister}>
+
+            <div className="auth__alert-error">
+                Hello guys
+            </div>
+
             <input 
             type="text"
             placeholder="Nombre"
