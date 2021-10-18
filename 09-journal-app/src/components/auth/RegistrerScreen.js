6980aabf34457f9ebee34a563a948f71d290c4de
Fernando <ferstxd@gmail.com>
2021-10-18 13:18:38 -0500

    Anadiendo useSelector

--- 09-journal-app/src/components/auth/RegistrerScreen.js
+++ 09-journal-app/src/components/auth/RegistrerScreen.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import validator from 'validator'
-import { useDispatch } from 'react-redux';
-import { types } from '../../types/types'
+import { useDispatch, useSelector } from 'react-redux';
 import { removeError, setError } from '../../actions/ui';
 
 
@@ -14,6 +13,10 @@ import { removeError, setError } from '../../actions/ui';
 export const RegistrerScreen = () => {
     
     const dispatch = useDispatch();
+    const {msgError} = useSelector( state => state.ui );
+    console.log( msgError);
+    
+
 
     const [formValues, handleInputChange] = useForm({
 
@@ -72,9 +75,13 @@ const isFormValid = () => {
         <h3 className="auth__title">Register</h3>
         <form onSubmit={handleRegister}>
 
-            <div className="auth__alert-error">
-                Hello guys
-            </div>
+           {   
+            msgError && 
+                (<div className="auth__alert-error">
+                    {msgError}
+                </div>)
+            
+            }
 
             <input 
             type="text"
