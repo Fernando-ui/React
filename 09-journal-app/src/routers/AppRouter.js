682f5bd7cc4f715010858c20fddfe610c31996fc
Fernando <ferstxd@gmail.com>
2021-10-21 12:19:17 -0500

    Anadiendo loading global

--- 09-journal-app/src/routers/AppRouter.js
+++ 09-journal-app/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,19 +8,46 @@ import {
     } from "react-router-dom";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
+import { firebaseApp } from '../firebase/firebase-config';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from "react-redux";
+import { login } from "../actions/auth";
 
 
-    export const AppRouter = () => {
+export const AppRouter = () => {
+    const auth = getAuth(firebaseApp);
+    const dispatch = useDispatch();
+
+    const [checking, setChecking] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) =>{
+            if(user?.uid){
+                dispatch(login(user.uid, user.displayName));
+                setIsLoggedIn( true );
+            }else {
+                setIsLoggedIn( false );
+            }
+            setChecking(false)
+        });
+    }, [auth,dispatch, setChecking,setIsLoggedIn]);
+        
+    if(checking){
+        return(
+            <h1>Espere...</h1>
+        )
+    }
+
     return (
-        <Router>
+      <Router>
         <div>
-
-            <Switch>
-                <Route path="/auth" component={AuthRouter}/>
-                <Route exact path="/" component={JournalScreen} />
-                <Redirect to="/auth/login"/>
-            </Switch>
+          <Switch>
+            <Route path="/auth" component={AuthRouter} />
+            <Route exact path="/" component={JournalScreen} />
+            <Redirect to="/auth/login" />
+          </Switch>
         </div>
-        </Router>
+      </Router>
     );
 }
