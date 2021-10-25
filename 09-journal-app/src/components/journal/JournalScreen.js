f73f3ce80afb807ba74a11cfd4072abf74fcd9fb
Fernando <ferstxd@gmail.com>
2021-10-25 11:36:21 -0500

    Anadiendo una nueva nota

--- 09-journal-app/src/components/journal/JournalScreen.js
+++ 09-journal-app/src/components/journal/JournalScreen.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NoteScreen } from '../notes/NoteScreen';
-// import { NothigSelected } from './NothigSelected';
+import { NothigSelected } from './NothigSelected';
 import { Sidebar } from './Sidebar';
 
 export const JournalScreen = () => {
+    const { active } = useSelector(state => state.notes);
+    console.log(active,'Tenemos el state de jorunal');
+
     return (
         <div className="journal__main-content">
             <Sidebar
              />
 
             <main>
-                {/* <NothigSelected /> */}
-                <NoteScreen />
+                {
+                    ( active ) ? ( <NoteScreen/>) : (<NothigSelected/>)
+                }
             </main>
         </div>
     )
