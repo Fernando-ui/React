c06fe4e9197d45845d25abb7850fcc14888b12ee
Fernando <ferstxd@gmail.com>
2021-10-25 15:19:39 -0500

    Anadiendo redux a nota

--- 09-journal-app/src/actions/notes.js
+++ 09-journal-app/src/actions/notes.js
@@ -1,27 +1,29 @@
 import {firebaseApp} from '../firebase/firebase-config';
 import { getFirestore, doc,  setDoc } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth'
+import { types } from '../types/types';
+
 
 
 export const startNewNote = () => {
     return async( dispatch, getState ) => {
         const firestore = getFirestore(firebaseApp);
-        
         const uid = getState().auth.uid;
         const newNote = {
             title:'',
             body:'',
             date: new Date().getTime(),
         }
-        console.log(uid);
-        
         let docuRef = doc(firestore,`${uid}/notes/`);
-        console.log(docuRef);
-        
-        const algo = await setDoc(docuRef,newNote);
-        console.log(algo);
-        
-        
+        await setDoc(docuRef, newNote);
+        dispatch(activeNote(docuRef.parent.id, newNote));
     }
 }
+
+export const activeNote = (id, note) => ({
+    type:types.notesActive,
+    payload:{
+        id,
+        ...note
+    }
+})
 
