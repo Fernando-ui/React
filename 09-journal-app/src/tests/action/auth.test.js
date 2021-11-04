8a31c4bd84a8bb2a14492da37388ff9ed976c431
Fernando <ferstxd@gmail.com>
2021-11-04 15:16:43 -0600

    Prueba con startlogut

--- 09-journal-app/src/tests/action/auth.test.js
+++ 09-journal-app/src/tests/action/auth.test.js
@@ -1,7 +1,16 @@
-import { login, logout } from '../../actions/auth'
+import { login, logout } from '../../actions/auth';
+import thunk from 'redux-thunk';
 import { types } from '../../types/types';
+import '@testing-library/jest-dom';
+
+let store  = mockStore(initState);
 
 describe('Pruebas con as accionees de Auth', () => {
+
+    beforeEach(() => {
+        store = mockStore( initState );
+    })
+
     test('login logout debe de crear la accion respectiva', () => {
         const uid = 'ABC123';
         const displayName = 'Fernando';
@@ -21,7 +30,27 @@ describe('Pruebas con as accionees de Auth', () => {
         });
     });
     
-    test(' debe de realizar el logout', () => {
+    test(" debe de realizar el startLogout", async () => {
+      await store.dispatch(startLogout());
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: types.logout,
+      });
+      expect(actions[1]).toEqual({
+        type: types.logout,
+      });
+    });
+    test('debe de iniciar el startLoginEmai.Password', async() => {
+        await store.dispatch( startLoginEmailPassword('test@testing.com','123456'));
+        const actions = store.getActions();
+        console.log(actions);
+        expect( action[1]).toEqual({
+            type:types.login,
+            payload:{
+                uid:'lkasdjfi3klkj',
+                displayName:null,  
+            }
+        })
         
     });
     
