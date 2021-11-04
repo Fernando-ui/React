d67217f7857f78a45554a5eaf9fc5051a8137d1b
Fernando <ferstxd@gmail.com>
2021-11-04 11:10:58 -0600

    Anadiendo variables de entorno

--- 09-journal-app/src/firebase/firebase-config.js
+++ 09-journal-app/src/firebase/firebase-config.js
@@ -5,26 +5,19 @@ import { GoogleAuthProvider } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfigTest = {
-  apiKey: "AIzaSyBwFADjsRSFnOwYhA9ZxUZlE3gbQS9meVw",
-  authDomain: "sql-demos-45685.firebaseapp.com",
-  projectId: "sql-demos-45685",
-  storageBucket: "sql-demos-45685.appspot.com",
-  messagingSenderId: "815996064025",
-  appId: "1:815996064025:web:76ba03375391913339b588",
-  measurementId: "G-CRC4CCDXD0"
-};  
-const firebaseConfig = {
-  apiKey: "AIzaSyBOFxp4N4vuqlf0JU9HETu1IaL_inOsBfs",
-  authDomain: "react-app-curso-ad470.firebaseapp.com",
-  projectId: "react-app-curso-ad470",
-  storageBucket: "react-app-curso-ad470.appspot.com",
-  messagingSenderId: "211651481996",
-  appId: "1:211651481996:web:6a9971d6d1c590a84146a9"
-};
-const firebaseApp = (process.env.NODE_ENV === 'test') ? initializeApp(firebaseConfigTest) : initializeApp(firebaseConfig);
 
-// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID,
+  appId:process.env.REACT_APP_APPID
+};
+
+const firebaseApp = initializeApp( firebaseConfig );
+
+// *Initialize Firebase
 const googleProvider = new GoogleAuthProvider();
 
 export{
