037363e2f8daeb5410ba41a4e93f1c7e2d455bb3
Fernando <ferstxd@gmail.com>
2021-11-08 10:22:07 -0600

    Cambios menores

--- 08-heroesApp/heroes-app/src/routers/AppRouter.js
+++ 08-heroesApp/heroes-app/src/routers/AppRouter.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import {
     BrowserRouter as Router,
     Switch,
-   
+    
     
 } from "react-router-dom";
 import { AuthContext } from "../auth/AuthContext";
