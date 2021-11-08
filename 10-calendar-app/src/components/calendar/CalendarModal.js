5cc15341c79651d1a902281ef7c354f5126cfcad
Fernando <ferstxd@gmail.com>
2021-11-08 15:50:31 -0600

    colocando fechas maximas y minimas

--- 10-calendar-app/src/components/calendar/CalendarModal.js
+++ 10-calendar-app/src/components/calendar/CalendarModal.js
@@ -1,42 +1,110 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
+import DateTimePicker from 'react-datetime-picker';
+import moment from "moment";
 
 const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 // * Hacemos referencia a nuestro componente div inicial
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
+const now = moment().minutes(0).seconds(0).add(1,'hours');
+
+// ? Anadiendo una hora mas a la hora actual
+const nowPlus1 = now.clone().add(1, 'hours');
+
 
 export const CalendarModal = () => {
-    const [isOpen, setIsOpen] = useState(true)
+  const [dateStart, setdateStart] = useState( now.toDate() );
+  console.log(dateStart);
+  
+  const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
 
-    const closeModal = () => {
-        setIsOpen(false)
-    }
-    
-    
+  const closeModal = () => {
+
+  };
+
+  const handleStartDateChange = (e) => {
+    setdateStart( e )
+    console.log(e);
+ }
+ const handleEndDateChange = ( e ) => {
+  setDateEnd( e );
+  console.log( e );
+  
+ }
+ 
+ 
+
   return (
     <>
       <Modal
-    //  isOpen Nos permitira si se muestra o no el modal: bool
-        isOpen={ isOpen }
+        // ? isOpen Nos permitira si se muestra o no el modal: bool
+        isOpen={true}
         // onAfterOpen={afterOpenModal}
-        onRequestClose={ closeModal }
-        closeTimeoutMS = { 200 }
+        onRequestClose={closeModal}
+        closeTimeoutMS={200}
         style={customStyles}
         className="modal"
         overlayClassName="modal-fondo"
       >
-          <h1>Hola mundo</h1>
-          <hr/>
-          <span>Hola de nuevo...</span>
+        <h1> Nuevo evento </h1>
+        <hr />
+        <form className="container">
+          <div className="form-group">
+            <label>Fecha y hora inicio</label>
+            <DateTimePicker
+              onChange={handleStartDateChange}
+              value={dateStart}
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Fecha y hora fin</label>
+             <DateTimePicker
+              onChange={handleEndDateChange}
+              value={dateEnd}
+              className="form-control"
+              //? Podemos colocar fechas minimas a escoger
+              minDate={ dateStart }
+            />
+          </div>
+
+          <hr />
+          <div className="form-group">
+            <label>Titulo y notas</label>
+           
+            <small id="emailHelp" className="form-text text-muted">
+              Una descripción corta
+            </small>
+          </div>
+
+          <div className="form-group">
+            <textarea
+              type="text"
+              className="form-control"
+              placeholder="Notas"
+              rows="5"
+              name="notes"
+            ></textarea>
+            <small id="emailHelp" className="form-text text-muted">
+              Información adicional
+            </small>
+          </div>
+
+          <button type="submit" className="btn btn-outline-primary btn-block">
+            <i className="far fa-save"></i>
+            <span> Guardar</span>
+          </button>
+        </form>
       </Modal>
     </>
   );
