5ac1cb7f40f10a08f0ed9af93bcf449392af0da0
Fersan <fst-96@hotmail.com>
2021-12-16 20:33:17 -0600

    Anadiendo rutas

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,21 +1,34 @@
 const express = require("express");
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = express.response) => {
     const {name, email, password} = req.body;
 
-    if(name.length < 5){
-        return res.status(400).json({
-            ok:false,
-            msg:'El nombre debe de ser mayor a 5'
-        });
-    }
+    // * Manejo de errores 
+  
+    const errors = validationResult( req);
+    console.log(errors);
+  if( !errors.isEmpty()){
+    return res.status(400).json({
+      ok:false,
+      errors:errors.mapped()
+    });
+  }
+    // if(name.length < 5){
+    //     return res.status(400).json({
+    //         ok:false,
+    //         msg:'El nombre debe de ser mayor a 5'
+    //     });
+    
+    // }
+
 
   res.json({
     ok: true,
     creando:'Usuario',
     name,
     email,
-    contrasenia,
+    password,
   });
 };
 
--- 11-calendar-backend/routes/auth.js
+++ 11-calendar-backend/routes/auth.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const router = Router();
 
 router.post('/',loginUsuario);
-router.post('/new', crearUsuario );  
+router.post('/new',
+    [
+        check('name','Debe de contener el nombre, es obligatorio').not().isEmpty()
+    ],
+ crearUsuario );  
 router.get('/renew', revalidarToken);  
 
 module.exports = router;
