6c93bccdf67b865869631bcd4a51c0650ac861d4
Fernando <fst_96@hotmail.com>
2022-01-11 09:28:24 -0600

    Encriptando la contrasenia

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 const Usuario = require("../models/Usuario");
+const bcryptjs = require('bcryptjs');
 
 const crearUsuario = async (req, res = express.response) => {
   const { email, password } = req.body;
 
   try {
     let usuario = await Usuario.findOne({ email });
-    if( usuario ){
+    if (usuario) {
       return res.status(400).json({
-        ok:false,
-        msg:'Un usuario existe con ese correo' 
+        ok: false,
+        msg: "Un usuario existe con ese correo",
       });
-    };
-      usuario = new Usuario( req.body); 
+    }
+    usuario = new Usuario(req.body);
+
+    // * Encriptar contraseña 
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt);
+
     await usuario.save();
 
     res.status(201).json({
       ok: true,
-      uid:usuario.id,
-      name:usuario.name
+      uid: usuario.id,
+      name: usuario.name,
     });
-
-
   } catch (error) {
     res.status(500).json({
       ok: false,
