6b8f750e0fcc73969a9935831279986f020d6eb2
Fernando <fst_96@hotmail.com>
2022-01-14 11:54:05 -0600

    Eliminando evento

--- 11-calendar-backend/controllers/events.js
+++ 11-calendar-backend/controllers/events.js
@@ -43,7 +43,7 @@ const actualizarEvento = async (req, res = response) => {
   try {
     const evento = await Evento.findById( eventoId );
     if( !evento ){
-      res.status(404).json({
+      return res.status(404).json({
         ok:false,
         msg:'Evento no existe por ese id'
       })
@@ -74,11 +74,38 @@ const actualizarEvento = async (req, res = response) => {
   }
     
 }
-const eliminarEvento = (req, res = response) => {
+const eliminarEvento = async(req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+    try {
+      const evento = await Evento.findById( eventoId );
+      if(!evento){
+        res.status(400).json({
+          ok:false,
+          msg:'Evento no existe por ese id'
+        })
+      }
+      if( evento.user.toString() !== uid){
+        return res.status(401).json({
+          ok:false,
+          msg:'No tiene privilegio de eliminar este evento'
+        });
+      }
+      const eventoBorrado = await Evento.findByIdAndDelete(eventoId);
     res.json({
-        ok:true,
-        msg:'Llamada a eliminarEvento'
+      ok:true,
+      msg:'Se ha borrado exitosamente',
+      evento:eventoBorrado
     })
+    } catch (error) {
+      console.log(error);
+      
+      return res.status(500).json({
+          ok:false,
+          msg:'Llame al administrador' 
+      })
+    }
 }
 
 module.exports = {
